0a3de15b7e636ced5189d993721bad3e58838b4e
Aminu Bakori <aminu@payant.ng>
2021-02-20 21:38:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+variable "token" {
+  type = "string"
+}
+
+variable "workspace_name" {
+  type = "string"
+}
+
+variable "organization_name" {
+  type = "string"
+}
+
 provider "tfe" {
   token    = var.token
   version  = "~> 0.15.0"
